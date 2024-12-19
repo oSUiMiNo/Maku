d26a7fd593cff9b392b72c1f360ef031b0834b76
t170d025@gunma-u.ac.jp <t170d025@gunma-u.ac.jp>
2024-12-19 18:19:53 +0900

    a

--- Universal/MyExtention.cs
+++ Universal/MyExtention.cs
@@ -483,6 +483,25 @@ public static class ObservableExtensions
     }
 }
 
+public static class ListExtentions
+{
+    public static List<int> Mode(this List<int> numbers)
+    {
+        var groupedNumbers = numbers.GroupBy(n => n)
+                                    .Select(g => new { Number = g.Key, Count = g.Count() })
+                                    .OrderByDescending(g => g.Count);
+
+        int maxCount = groupedNumbers.First().Count;
+
+        List<int> modes = groupedNumbers.Where(g => g.Count == maxCount)
+                                  .Select(g => g.Number).ToList();
+
+        Debug.Log("ç≈ïpíl: " + string.Join(", ", modes));
+
+        return modes;
+    }
+}
+
 
 
 public static class RectTransformExtensions

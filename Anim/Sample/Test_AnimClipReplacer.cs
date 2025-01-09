cb33ccc6828d08f8fac8d68750bcacf3a43d6770
t170d025@gunma-u.ac.jp <t170d025@gunma-u.ac.jp>
2025-01-10 03:56:28 +0900

    a

--- Anim/Sample/Test_AnimClipReplacer.cs
+++ Anim/Sample/Test_AnimClipReplacer.cs
@@ -1,54 +1,54 @@
-using UnityEngine;
-using UnityEngine.AddressableAssets;
+//using UnityEngine;
+//using UnityEngine.AddressableAssets;
 
-public class Test_AnimClipReplacer : MonoBehaviour
-{
-    [SerializeField] Animator animator;
-    [SerializeField] AnimationClip clip_Run;
-    [SerializeField] AnimationClip clip_SitDown;
-    AnimClipReplacer replacer;
-
-
-    void Start()
-    {
-        replacer = new AnimClipReplacer(animator, "RUN00_F");
-    }
+//public class Test_AnimClipReplacer : MonoBehaviour
+//{
+//    [SerializeField] Animator animator;
+//    [SerializeField] AnimationClip clip_Run;
+//    [SerializeField] AnimationClip clip_SitDown;
+//    AnimClipReplacer replacer;
 
 
-    async void Update()
-    {
-        // ステート遷移
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            if (animator.GetInteger("State") == 0)
-                animator.SetInteger("State", 1);
-            else
-            if (animator.GetInteger("State") == 1)
-                animator.SetInteger("State", 0);
-        }
-        else
-        // クリップ差し替え
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            if (replacer.Exe(clip_Run))
-                Debug.Log($"差し替え：ステート[RUN00_F] <== クリップ[RUN00_F]");
-            else
-            if (replacer.Exe(clip_SitDown))
-                Debug.Log($"差し替え：ステート[RUN00_F] <== クリップ[SitDown]");
-        }
-        else
-        // AddressableAsset化したクリップ差し替え
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            if (replacer.Exe(await Addressables.LoadAssetAsync<AnimationClip>("SitDown").Task))
-            {
-                Debug.Log($"差し替え：ステート[RUN00_F] <== Addressable[SitDown]");
-            }
-            else
-            {
-                replacer.Reset();
-                Debug.Log($"差し替え：ステート[RUN00_F] <== リセット");
-            }
-        }
-    }
-}
+//    void Start()
+//    {
+//        replacer = new AnimClipReplacer(animator, "RUN00_F");
+//    }
+
+
+//    async void Update()
+//    {
+//        // ステート遷移
+//        if (Input.GetKeyDown(KeyCode.A))
+//        {
+//            if (animator.GetInteger("State") == 0)
+//                animator.SetInteger("State", 1);
+//            else
+//            if (animator.GetInteger("State") == 1)
+//                animator.SetInteger("State", 0);
+//        }
+//        else
+//        // クリップ差し替え
+//        if (Input.GetKeyDown(KeyCode.S))
+//        {
+//            if (replacer.Exe(clip_Run))
+//                Debug.Log($"差し替え：ステート[RUN00_F] <== クリップ[RUN00_F]");
+//            else
+//            if (replacer.Exe(clip_SitDown))
+//                Debug.Log($"差し替え：ステート[RUN00_F] <== クリップ[SitDown]");
+//        }
+//        else
+//        // AddressableAsset化したクリップ差し替え
+//        if (Input.GetKeyDown(KeyCode.D))
+//        {
+//            if (replacer.Exe(await Addressables.LoadAssetAsync<AnimationClip>("SitDown").Task))
+//            {
+//                Debug.Log($"差し替え：ステート[RUN00_F] <== Addressable[SitDown]");
+//            }
+//            else
+//            {
+//                replacer.Reset();
+//                Debug.Log($"差し替え：ステート[RUN00_F] <== リセット");
+//            }
+//        }
+//    }
+//}

b2621a64c74fb023402e71becdcbae79702aa14a
t170d025@gunma-u.ac.jp <t170d025@gunma-u.ac.jp>
2025-01-13 05:00:36 +0900

    a

--- PythonAPI/PythonAssets/PyAPI.py
+++ PythonAPI/PythonAssets/PyAPI.py
@@ -1,20 +1,22 @@
 import sys
 import json
 import os
+from inspect import stack
+from pathlib import Path
 
 
 # 更新するファイルのパス
 LogPath = ""
+RootPah = ""
 
 
 def APInit():
-    global LogPath
+    global LogPath, RootPah
     # Assets 直下にログ用txtファイルがある
-    LogPath = f"{os.getcwd()}/Assets/PyLog.txt"
-    Log(f"カレント {os.getcwd()}")
+    LogPath = f"{Path.cwd()}/Assets/PyLog.txt"
+    RootPah = Path.cwd()
     # 自分が配置されているディレクトリに移動
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    Log(f"カレント {os.getcwd()}")
     
 
 
@@ -41,10 +43,19 @@ def APIOut(outputJobj):
 
 def Log(msg):
     if os.path.exists(LogPath):
-        # print("ある")
+        # スタックの呼び出し元情報を取得（スタックの1つ上）
+        caller_frame = stack()[1]
+        # 呼び出し元ファイルの相対パス
+        caller_file = Path(caller_frame.filename).relative_to(RootPah).as_posix()
+        # 呼び出し元の行番号
+        caller_line = caller_frame.lineno
         # ファイルに追記
         with open(LogPath, "a", encoding="utf-8") as file:
-            file.write(f"___\n{msg}\n")
+            file.write( 
+                        f"___\n{msg}\n"
+                        f"./{caller_file}:{caller_line}")
     else:
         # ファイルが存在しない場合はコンソールに出力
         print(msg)
+
+

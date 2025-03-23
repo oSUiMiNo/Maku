c5147a1bd76ba12fd1f1b5bd3161d749ff3b0bd7
t170d025@gunma-u.ac.jp <t170d025@gunma-u.ac.jp>
2025-03-23 11:04:07 +0900

    a

--- Universal/ColorUtil.cs
+++ Universal/ColorUtil.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.Experimental.GlobalIllumination;
-public class ColorConverter
+public class ColorUtil
 {
-    public Color HEX2RGBA(string hex)
+    public static Color HEX(string hex)
     {
         if (!hex.Contains("#")) hex = "#" + hex;
 

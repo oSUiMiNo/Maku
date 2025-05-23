656827b140ff3345e5292057947b86a2f51f440f
t170d025@gunma-u.ac.jp <t170d025@gunma-u.ac.jp>
2025-05-23 23:04:50 +0900

    a

--- WindowSizer/WindowSizer.cs
+++ WindowSizer/WindowSizer.cs
@@ -25,10 +25,12 @@ public class WindowSizer : MonoBehaviour
     {
         Debug.Log($"{listNum}");
         if (listNum == 0)
-            Screen.SetResolution(1280, 720, FullScreenMode.Windowed);
+            Screen.SetResolution(720, 480, FullScreenMode.Windowed);
         if (listNum == 1)
-            Screen.SetResolution(1920, 1080, FullScreenMode.Windowed);
+            Screen.SetResolution(1280, 720, FullScreenMode.Windowed);
         if (listNum == 2)
+            Screen.SetResolution(1920, 1080, FullScreenMode.Windowed);
+        if (listNum == 3)
             Screen.SetResolution(3840, 2160, FullScreenMode.Windowed);
         
         if(Txt) Txt.text = $"{Screen.width} Å~ {Screen.height}";

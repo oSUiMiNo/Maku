3a661cc24bcec31056f0663cbcbecb81005876ad
t170d025@gunma-u.ac.jp <t170d025@gunma-u.ac.jp>
2025-05-23 19:01:23 +0900

    a

---
+++ WindowSizer/WindowSizer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UniRx;
+
+
+public class WindowSizer : MonoBehaviour
+{
+    TMP_Dropdown Dropdown => transform.Find("Dropdown").GetComponent<TMP_Dropdown>();
+    TextMeshProUGUI Txt => transform.Find("Text").GetComponent<TextMeshProUGUI>();
+
+   
+    void Start()
+    {
+        SetSize(Dropdown.value);
+        Dropdown.onValueChanged.AsObservable().Subscribe(value =>
+        {
+            SetSize(value);
+        });
+    }
+
+    void Update()
+    {
+        Txt.text = $"Current Resolution: {Screen.width} Å~ {Screen.height}";
+    }
+
+    void SetSize(int listNum)
+    {
+        Debug.Log($"{listNum}");
+        if (listNum == 0)
+            Screen.SetResolution(1280, 720, FullScreenMode.Windowed);
+        if (listNum == 1)
+            Screen.SetResolution(1920, 1080, FullScreenMode.Windowed);
+        if (listNum == 2)
+            Screen.SetResolution(3840, 2160, FullScreenMode.Windowed);
+    }
+}

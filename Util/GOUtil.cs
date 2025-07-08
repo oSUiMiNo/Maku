f05160b868569b763115adfa28c188eea9e3f8b1
TUF\user <osuimino@gmail.com>
2025-07-08 20:25:35 +0900

    a

--- Util/GOUtil.cs
+++ Util/GOUtil.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
 
 
 namespace MyUtil
